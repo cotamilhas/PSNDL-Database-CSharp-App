b55ce45bd1a864b2fd29218d9ccebed1760a8a45
Henrique <Henrique@DESKTOP-32T0UMT>
2023-05-04 19:44:46 +0100

    Adicionar arquivos de projeto.

---
+++ PSNDLdb/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PSNDLdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 81);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1712, 666);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1476, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download PKG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(80, 49);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(283, 22);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(372, 49);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(27, 25);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1607, 49);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Download RAP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(-4, -4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1751, 34);
+            this.panel1.TabIndex = 7;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(1711, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 32);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PPSNDL";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1744, 758);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PPSNDL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PSNDLdb/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Runtime.InteropServices;
+
+namespace PSNDLdb
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int r, g, b;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1; 
+        public const int HT_CAPTION = 0x2; 
+        [DllImportAttribute("user32.dll")] 
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam); 
+        [DllImportAttribute("user32.dll")] 
+        public static extern bool ReleaseCapture();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 50;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+            r = 0;
+            g = 0;
+            b = 0;
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.VirtualMode = false;
+            LoadTXT();
+            
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void Search(string name)
+        {
+            // Itera sobre as linhas do DataGridView
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                // Verifica se a célula na coluna "Name" contém o nome pesquisado
+                if (row.Cells["Name"].Value != null && row.Cells["Name"].Value.ToString().Contains(name))
+                {
+                    // Se sim, torna a linha visível
+                    row.Visible = true;
+                }
+                else
+                {
+                    row.Visible = false;
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format("Name LIKE '%{0}%'", txtSearch.Text);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadTXT()
+        {
+            if (File.Exists("db.txt"))
+            {
+                // Lê todas as linhas do arquivo
+                string[] lines = File.ReadAllLines("db.txt");
+
+                // Cria um DataTable para armazenar os dados
+                DataTable dt = new DataTable();
+
+                // Adiciona as colunas no DataTable
+                dt.Columns.Add("TitleID");
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Type");
+                dt.Columns.Add("Region");
+                dt.Columns.Add("PKGLink");
+                dt.Columns.Add("RAP");
+                dt.Columns.Add("ContentID");
+                dt.Columns.Add("Description");
+                dt.Columns.Add("Uploader");
+
+                // Adiciona as linhas no DataTable
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(';');
+
+                    // Verifica se o número de elementos na matriz data é igual ao número de colunas no DataTable
+                    if (data.Length != dt.Columns.Count)
+                    {
+                        // Trata o erro aqui, se necessário
+                        continue; // Pula para a próxima linha do arquivo
+                    }
+
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        if (string.IsNullOrEmpty(data[i]))
+                        {
+                            data[i] = "Unavailable";
+                        }
+                    }
+
+
+                    dt.Rows.Add(data);
+
+ 
+                    
+                }
+
+                // Define o DataTable como DataSource do DataGridView
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Database not found.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            string name = dataGridView1.Rows[selectedRowIndex].Cells["PKGLink"].Value.ToString();
+
+            System.Diagnostics.Process.Start(name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            string rap = dataGridView1.Rows[selectedRowIndex].Cells["RAP"].Value.ToString();
+            string contentid = dataGridView1.Rows[selectedRowIndex].Cells["ContentID"].Value.ToString();
+
+
+            string[] parts = rap.Split(new string[] { ".RAP" }, StringSplitOptions.None);
+            string output = parts[0];
+
+            string link = "https://nopaystation.com/tools/rap2file/";
+
+            System.Diagnostics.Process.Start(link + output + "/" + contentid);
+
+
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) 
+            { 
+                ReleaseCapture(); 
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0); 
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) 
+            { 
+                ReleaseCapture(); 
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0); 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.FromArgb(r, g, b);
+
+            r += 10;
+            g += 20;
+            b += 30;
+
+            if (r > 255) r = 0;
+            if (g > 255) g = 0;
+            if (b > 255) b = 0;
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format("Name LIKE '%{0}%'", txtSearch.Text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
